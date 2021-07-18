d7cd960387115e41aec90bf7c9a92cd1f9158095
Kotikgames <kotikgamesby@gmail.com>
2021-07-18 21:16:58 +0300

    printError(), exit()

--- src/main/kotlin/lclang/libs/std/StdLibrary.kt
+++ src/main/kotlin/lclang/libs/std/StdLibrary.kt
@@ -3,6 +3,8 @@ package lclang.libs.std
 import lclang.Type
 import lclang.libs.Library
 import lclang.methods.Method
+import kotlin.system.exitProcess
+import java.util.*
 
 class StdLibrary: Library() {
     init {
@@ -20,5 +22,25 @@ class StdLibrary: Library() {
                 return null
             }
         }
+
+        methods["readLine"] = object: Method(listOf(), Type.STRING) {
+            override fun call(args: List<Any?>): Any? {
+                val scanner = Scanner(System.`in`)
+                return scanner.nextLine()
+            }
+        }
+
+        methods["printError"] = object: Method(listOf(Type.ANY), Type.VOID) {
+            override fun call(args: List<Any?>): Any? {
+                error(args[0].toString());
+            }
+        }
+
+        methods["exit"] = object: Method(listOf(Type.INT), Type.VOID) {
+            override fun call(args: List<Any?>): Any? {
+                exitProcess(args[0] as Int);
+            }
+        }
+
     }
 }
