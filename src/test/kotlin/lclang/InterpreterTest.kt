e971ac4087aeb60e1211326672b12ec7c6341914
levkopo <40952805+levkopo@users.noreply.github.com>
2021-07-22 15:23:20 +0500

    Tester update

--- src/test/kotlin/lclang/InterpreterTest.kt
+++ src/test/kotlin/lclang/InterpreterTest.kt
@@ -4,52 +4,58 @@ import lclang.exceptions.LCLangException
 import lclang.methods.Method
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import java.io.File
-import java.lang.StringBuilder
+import java.util.stream.Stream
 
 class InterpreterTest {
-    val tests = File("./tests/")
+    private val testsFile = File("./tests/")
 
-    @Test
-    fun runTests() {
-        for(file in tests.listFiles()!!){
-            val contents = read(file).split(Regex("--OUTPUT--\\R"))
-            val fileScript = contents[0]
-            val needOutput = contents[1]
-            val output = StringBuilder()
+    @TestFactory
+    fun run(): Stream<DynamicTest> {
+        val tests = ArrayList<DynamicTest>()
+        for(file in testsFile.listFiles()!!){
+            tests.add(DynamicTest.dynamicTest(file.name) {
+                val contents = read(file).split(Regex("--OUTPUT--\\R"))
+                val fileScript = contents[0]
+                val needOutput = contents[1]
+                val output = StringBuilder()
 
-            try {
-                val lexer = lclangLexer(CharStreams.fromString(fileScript))
-                val tokens = CommonTokenStream(lexer)
-                val parser = lclangParser(tokens)
-                parser.removeErrorListeners()
-                parser.addErrorListener(ErrorListener(file.path.toString()))
+                try {
+                    val lexer = lclangLexer(CharStreams.fromString(fileScript))
+                    val tokens = CommonTokenStream(lexer)
+                    val parser = lclangParser(tokens)
+                    parser.removeErrorListeners()
+                    parser.addErrorListener(ErrorListener(file.path.toString()))
 
-                LCFileVisitor(file.path.toString()).apply {
-                    methods["println"] = object: Method(listOf(Type.ANY), Type.VOID) {
-                        override fun call(fileVisitor: LCFileVisitor, args: List<Any?>): Any? {
-                            output.append("${args[0]}\n")
-                            return null
+                    LCFileVisitor(file.path.toString()).apply {
+                        methods["println"] = object : Method(listOf(Type.ANY), Type.VOID) {
+                            override fun call(fileVisitor: LCFileVisitor, args: List<Any?>): Any? {
+                                output.append("${args[0]}\n")
+                                return null
+                            }
                         }
+
+                        methods["print"] = object : Method(listOf(Type.ANY), Type.VOID) {
+                            override fun call(fileVisitor: LCFileVisitor, args: List<Any?>): Any? {
+                                output.append(args[0])
+                                return null
+                            }
+                        }
+
+                        visitFile(parser.file())
                     }
 
-                    methods["print"] = object: Method(listOf(Type.ANY), Type.VOID) {
-                        override fun call(fileVisitor: LCFileVisitor, args: List<Any?>): Any? {
-                            output.append(args[0])
-                            return null
-                        }
-                    }
-
-                    visitFile(parser.file())
+                } catch (e: LCLangException) {
+                    output.append(e.message + "\n")
                 }
 
-            }catch (e: LCLangException){
-                output.append(e.message+"\n")
-            }
-
-            if(needOutput!=output.toString())
-                throw Exception("Invalid output in ${file.path}\nOutput: " + output.toString())
+                assertEquals(needOutput, output.toString())
+            })
         }
+
+        return tests.stream()
     }
 }
