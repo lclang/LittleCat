17ba53091dcd9906353e4fb32c72c4cdcdd2740e
Kotikgames <kotikgamesby@gmail.com>
2021-07-25 19:39:56 +0300

    array.join()

--- src/main/kotlin/lclang/ValueList.kt
+++ src/main/kotlin/lclang/ValueList.kt
@@ -1,11 +1,21 @@
 package lclang
 
+import lclang.methods.Method
+
 class ValueList(file: LCFileVisitor): LCClass("array", file) {
     val list = ArrayList<Value>()
 
     fun add(value: Value) = list.add(value)
     fun get(index: Int) = list[index]
     fun last() = list.last()
+
+    init{
+        methods["join"] = object: Method(listOf(Type.STRING), Type.ARRAY) {
+            override fun call(fileVisitor: LCFileVisitor, args: List<Any?>): Any {
+                return list.joinToString(args[0] as String)
+            }
+        }
+    }
 
     override fun toString(): String {
         var str = ""
