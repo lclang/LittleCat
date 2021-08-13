fdfe7b5b687088bb3e9ddc37478a5556617650e0
Лев Копорушкин <levkopoag@gmail.com>
2021-08-13 12:20:23 +0500

    Update LibraryMethod.kt

--- src/main/kotlin/lclang/methods/LibraryMethod.kt
+++ src/main/kotlin/lclang/methods/LibraryMethod.kt
@@ -2,6 +2,8 @@ package lclang.methods
 
 import lclang.LCFileVisitor
 import lclang.Value
+import lclang.lang.CharClass
+import lclang.lang.StringClass
 import lclang.types.BaseType
 
 abstract class LibraryMethod(args: List<BaseType>, returnType: BaseType) : Method(args, returnType) {
@@ -10,7 +12,11 @@ abstract class LibraryMethod(args: List<BaseType>, returnType: BaseType) : Metho
             if(it is Unit)
                 return null
             else it
-        }
+        }?.let { when(it) {
+            is Char -> CharClass(it, fileVisitor)
+            is String -> StringClass(it, fileVisitor)
+            else -> it
+        }}
     }
 
     abstract fun callMethod(fileVisitor: LCFileVisitor, args: List<Any?>): Any?
